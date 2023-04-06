18744aa04cb0f1b0f1d8ea7fb0d91fd07caaeaff
Dominik Bastian <dominik_bastian@web.de>
2023-04-07 00:59:13 +0200

    Fixed pages app

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -3,13 +3,6 @@ import {DotsGame, GameEngine} from "@doba16/predictable-dots-game"
 
 const canvas: HTMLCanvasElement = document.getElementById("test-canvas") as HTMLCanvasElement
 
-/* new DotsGame({
-    canvas,
-    width: 6,
-    height: 6
-}).startDrawing() */
-
-
 const engine = new GameEngine(canvas)
 engine.initialize()
 
--- pages-app/src/examples/simple-example.ts
+++ pages-app/src/examples/simple-example.ts
@@ -1,9 +1,12 @@
-import {DotsGame} from "@doba16/predictable-dots-game"
+import {DotsGame, GameEngine} from "@doba16/predictable-dots-game"
 
 const simpleExampleCanvas = document.querySelector("#simple-example-canvas") as HTMLCanvasElement
 
+const engine = new GameEngine(simpleExampleCanvas)
+engine.initialize()
+
 new DotsGame({
-    canvas: simpleExampleCanvas,
+    engine,
     width: 6,
     height: 6
-}).startDrawing()
+})
